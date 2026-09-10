2ffb885273ded69fb4e6bef03cd26b1231212525
alwinjoyal <alwin.joyal@cathcollege.school.nz>
2026-09-10 12:44:42 +1200

    new

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,7 +7,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var start_position = Vector2(40,480)
-
+var paused = false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
